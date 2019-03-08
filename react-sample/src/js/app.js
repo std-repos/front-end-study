a5fcabad6f5363cec07b92f2da3db6e371ddde55
F21\namhee.k <namhee.k@forever21.com>
2019-03-08 10:32:21 +0900

    변수니 뭐니 연습할려고

--- react-sample/src/js/app.js
+++ react-sample/src/js/app.js
@@ -4,9 +4,24 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import style from '../css/style';
 
-/*hello();*/
+hello();
 
 ReactDom.render(
     <h1>Hello world!!!Hello world!!!</h1>, 
+    //invalid();
     document.getElementById('root')
-);;
+    /*
+    
+    class Person {
+        constructor (id,name) {
+            this.id = id;
+            this.name = name;
+        }
+        printName() {
+            console.log(this.name);
+        }
+    }
+     var person = new Person(1, "철수");
+     person.printName();
+    */
+);
