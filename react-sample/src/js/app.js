36f65985dcd30ccfa69dc462a2f673531809c56b
lodossw <lee_jongseok01@eland.co.kr>
2019-03-08 13:40:59 +0900

    한번더 app.js 푸시

--- react-sample/src/js/app.js
+++ react-sample/src/js/app.js
@@ -6,10 +6,11 @@ import ReactDom from 'react-dom';
 import style from '../css/style';
 import Person from './person';
 
+
+
 hello();
 
 ReactDom.render(
     <h1>Hello world!!!Hello world!!!</h1>, 
     document.getElementById('root')
 );
-
