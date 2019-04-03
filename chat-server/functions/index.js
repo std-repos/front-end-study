a6c104f2954baa66211698d500144a6acb9cdc11
F21\namhee.k <namhee.k@forever21.com>
2019-04-03 11:19:12 +0900

    한번 돌려봐 타임아웃이라고 에러날거야 신텍스 에러도 있는거 같은데... 원본에서 가져다 붙였거든(신텍스에러는 아닌거 같아)

--- chat-server/functions/index.js
+++ chat-server/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// /A/ https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
@@ -13,50 +13,47 @@ admin.initializeApp(functions.config().firebase);
 const express = require('express');
 const app = express();
 
-const cors= require('cors') ({origin:true});
+const cors = require('cors')({origin: true});
 app.use(cors);
 
 const anonymousUser = {
-   id: "anon", 
-   name: "Anonymous",
-   avatar:""
-}
+    id: "anon",
+    name: "Anonymous",
+    avatar: ""
+};
 
-
-//사용자 정보 알아내기
-const checkUser = (req,res,next) => {
+const checkUser = (req, res, next) => {
     req.user = anonymousUser;
-
-    if(req.query.auth_token != undefined) {
+    if (req.query.auth_token !== undefined) {
         let idToken = req.query.auth_token;
         admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
-            let authUser =  {
-                id : decodedIdToken.user_id,
-                name :decodedIdToken.name,
-                avatar :decodedIdToken.picture
+            let authUser = {
+                id: decodedIdToken.user_id,
+                name: decodedIdToken.name,
+                avatar: decodedIdToken.picture
             };
             req.user = authUser;
             next();
         }).catch(error => {
             next();
         });
+    } else {
+        next();
     };
-    app.use(checkUser); //순서 확인 p106
 };
 
-//채널 생성 API
-function createChannel(cname) {
-    let channelsRef = admin.datebase().ref('channels');
+app.use(checkUser);
+
+function createChannel(cname){
+    let channelsRef = admin.database().ref('channels');
     let date1 = new Date();
     let date2 = new Date();
-    date2.setSeconds(date2.getSeconds() + 1 );
-
-    
+    date2.setSeconds(date2.getSeconds() + 1);
     const defaultData = `{
-        "message" : {
+        "messages" : {
             "1" : {
                 "body" : "Welcome to #${cname} channel!",
-                "date" : "${data1.toJSON()}",
+                "date" : "${date1.toJSON()}",
                 "user" : {
                     "avatar" : "",
                     "id" : "robot",
@@ -64,8 +61,8 @@ function createChannel(cname) {
                 }
             },
             "2" : {
-                "body" : "첫번째 메세지를 보내봅시다!",
-                "date" : "${data2.toJSON()}",
+                "body" : "はじめてのメッセージを投稿してみましょう。",
+                "date" : "${date2.toJSON()}",
                 "user" : {
                     "avatar" : "",
                     "id" : "robot",
@@ -77,48 +74,43 @@ function createChannel(cname) {
     channelsRef.child(cname).set(JSON.parse(defaultData));
 }
 
-app.post('/channels',(req, res) => {
+app.post('/channels', (req, res) => {
     let cname = req.body.cname;
     createChannel(cname);
-    res.header('Content-Type','application/json; charset=utf-8');
+    res.header('Content-Type', 'application/json; charset=utf-8');
     res.status(201).json({result: 'ok'});
 });
 
-//목호 확인하는 API
-app.get('/channels',(req,res) => {
+app.get('/channels', (req, res) => {
     let channelsRef = admin.database().ref('channels');
-    channelsRef.once('value', function(snapshot){
-      let items = new Array();
-      snapshot.forEach(function(childSnapshot){
-          let cname = childSnapshot.key;
-          items.push(cname);
-      });
-      res.header('Content-Type','application/json; charset=utf-8');
-      res.send({channels: items});
-    })
+    channelsRef.once('value', function(snapshot) {
+        let items = new Array();
+        snapshot.forEach(function(childSnapshot) {
+            let cname = childSnapshot.key;
+            items.push(cname);
+        });
+        res.header('Content-Type', 'application/json; charset=utf-8');
+        res.send({channels: items});
+    });
 });
 
-//지정한 채널에 새 메세지를 추가하는 API
-app.post('/channels/:cname/messages', (req,res) => {
+app.post('/channels/:cname/messages', (req, res) => {
     let cname = req.params.cname;
     let message = {
-        date : new Date().toJSON(),
-        body:req.bodybody,
-        user:req.user
+        date: new Date().toJSON(),
+        body: req.body.body,
+        user: req.user
     };
     let messagesRef = admin.database().ref(`channels/${cname}/messages`);
-    messagesRef.push(messages);
-    res.header('Content-Type','application/json; charset=utf-8');
-    res.status(201).send({result: 'ok'});
-
+    messagesRef.push(message);
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.status(201).send({result: "ok"});
 });
 
-// 채널내 메세지 목록 확인 API 
-app.get('/channels/:cname/messages', (req,res) => {
+app.get('/channels/:cname/messages', (req, res) => {
     let cname = req.params.cname;
-    let messagesRef = admin.database().ref(`channels/${cname}/messages`);
-    orderByChild('date').limitToLast(20);
-    messagesRef.once('value', function(snapshot){
+    let messagesRef = admin.database().ref(`channels/${cname}/messages`).orderByChild('date').limitToLast(20);
+    messagesRef.once('value', function(snapshot) {
         let items = new Array();
         snapshot.forEach(function(childSnapshot) {
             let message = childSnapshot.val();
@@ -126,17 +118,17 @@ app.get('/channels/:cname/messages', (req,res) => {
             items.push(message);
         });
         items.reverse();
-        res.header('Content-Type','application/json; charset=utf-8');
+        res.header('Content-Type', 'application/json; charset=utf-8');
         res.send({messages: items});
-    }) 
+    });
 });
 
-//초기 상태로 되돌리기
-app.post('/reset', (req,res) => {
+app.post('/reset', (req, res) => {
     createChannel('general');
     createChannel('random');
-    res.header('Content-Type','application/json; charset=utf-8');
-    res.status(201).send({result: 'ok'});
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.status(201).send({result: "ok"});
 });
 
-exports.v1= functions.https.onRequest(app);
+exports.v1 = functions.https.onRequest(app);
+
